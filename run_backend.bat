4cf6fc27719a0867263827e711a64ff56ba83929
dulgiDove <k1010282@naver.com>
2026-05-16 18:32:31 +0900

    빌드 파일 및 스크립트 제거

--- run_backend.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-wsl bash /mnt/c/Users/pc123/HumanoidVerse/run_backend.sh
-pause
--- run_backend.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source /home/k1010282/miniconda3/etc/profile.d/conda.sh
-conda activate hgen
-cd /mnt/c/Users/pc123/HumanoidVerse
-export LD_LIBRARY_PATH=/usr/lib/wsl/lib:$LD_LIBRARY_PATH
-python humanoidverse/eval_agent.py "+checkpoint=logs/H1_Stage3/20260513_033805-H1_10dof_obstacle_v7-locomotion-h1_10dof/model_75700.pt" +headless=True +num_envs=1
